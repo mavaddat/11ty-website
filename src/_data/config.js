593f678b924dba704994db3b571b02d63d793ae0
Zach Leatherman <zachleat@users.noreply.github.com>
2026-04-28 14:47:11 -0500

    Show time left on Kickstarter

--- src/_data/config.js
+++ src/_data/config.js
@@ -7,4 +7,5 @@ export default {
 	origin: "https://www.11ty.dev", // important: no trailing slash on URL
 	kickstarterUrl: "https://www.kickstarter.com/projects/fontawesome/build-awesome-pro?ref=43ttgb",
 	kickstarterLaunchDate: (new Date("2026-04-28T15:00:00Z")).toISOString(),
+	kickstarterCloseDate: (new Date("2026-05-28T21:00:00Z")).toISOString(),
 };
